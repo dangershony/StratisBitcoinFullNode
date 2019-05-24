e3a4ca512d0dbde6950849ab811775c6ecd90c42
aubergemediale <ce@aubergemediale.com>
2019-05-24 04:11:00 +0200

    Starting DroidD with 2 gateway nodes.

--- src/Obsidian.DroidD/NodeConfig.cs
+++ src/Obsidian.DroidD/NodeConfig.cs
@@ -2,7 +2,7 @@
 {
     public static class NodeConfig
     {
-        public static string[] StartParameters { get; set; } = new string[] { "-connect=165.22.90.248" };
+        public static string[] StartParameters { get; set; } = new string[] { "-addnode=165.22.90.248", "-addnode=185.64.104.55" };
 
         //var startParameters = new[] { "-addnode=165.22.90.248", "-maxoutboundconnections=2", "-maxinboundconnections=1" };
        
