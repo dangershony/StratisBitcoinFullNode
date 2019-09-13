c9ff6132ae80d026a946294227a9f658499885b2
Dan Gershony <dan.gershony@gmail.com>
2019-09-13 13:11:52 +0100

    Creating the feature

---
+++ src/Stratis.Bitcoin.Features.Airdrop/AirdropFeature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBitcoin;
+using Stratis.Bitcoin.Builder.Feature;
+using Stratis.Bitcoin.EventBus;
+using Stratis.Bitcoin.EventBus.CoreEvents;
+using Stratis.Bitcoin.Signals;
+
+namespace Stratis.Bitcoin.Features.Airdrop
+{
+    public class AirdropFeature : FullNodeFeature
+    {
+        private readonly Network network;
+        private readonly ISignals signals;
+        private readonly ChainIndexer chainIndexer;
+        private readonly AirdropSettings airdropSettings;
+        public string FilenamePrefix { get; set; }
+
+        private SubscriptionToken blockConnectedSubscription;
+
+        public AirdropFeature(Network network, ISignals signals, ChainIndexer chainIndexer, AirdropSettings airdropSettings)
+        {
+            this.network = network;
+            this.signals = signals;
+            this.chainIndexer = chainIndexer;
+            this.airdropSettings = airdropSettings;
+
+            if (airdropSettings.SnapshotHeight != null)
+            {
+                this.blockConnectedSubscription = this.signals.Subscribe<BlockConnected>(this.OnBlockConnected);
+            }
+        }
+
+        public override Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        private void OnBlockConnected(BlockConnected blockConnected)
+        {
+            if (blockConnected.ConnectedBlock.ChainedHeader.Height == this.airdropSettings.SnapshotHeight)
+            {
+                // Take a snapshot of the chain.
+            }
+        }
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.Airdrop/AirdropSettings.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.Bitcoin.Features.Airdrop
+{
+    /// <summary>
+    /// Configuration related to the the airdrop feature.
+    /// </summary>
+    public class AirdropSettings
+    {
+        /// <summary>Defines block height at wich to create the airdrop.</summary>
+        public int? SnapshotHeight { get; set; }
+
+        /// <summary>Instance logger.</summary>
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// Initializes an instance of the object from the node configuration.
+        /// </summary>
+        /// <param name="nodeSettings">The node configuration.</param>
+        public AirdropSettings(NodeSettings nodeSettings)
+        {
+            Guard.NotNull(nodeSettings, nameof(nodeSettings));
+            
+            this.logger = nodeSettings.LoggerFactory.CreateLogger(typeof(AirdropSettings).FullName);            
+            TextFileConfiguration config = nodeSettings.ConfigReader;
+            this.SnapshotHeight = config.GetOrDefault<int?>("snapshotheight", null, this.logger);
+        }
+
+        /// <summary>Prints the help information on how to configure the DNS settings to the logger.</summary>
+        /// <param name="network">The network to use.</param>
+        public static void PrintHelp(Network network)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine($"-snapshotheight=<0-max>   The height of the chain to take the snapshot form");
+
+            NodeSettings.Default(network).Logger.LogInformation(builder.ToString());
+        }
+
+        /// <summary>
+        /// Get the default configuration.
+        /// </summary>
+        /// <param name="builder">The string builder to add the settings to.</param>
+        /// <param name="network">The network to base the defaults off.</param>
+        public static void BuildDefaultConfigurationFile(StringBuilder builder, Network network)
+        {
+            builder.AppendLine("####Airdrop Settings####");
+            builder.AppendLine($"#The SnapshotHeight. Defaults to null disabled");
+        }
+    }
+}
