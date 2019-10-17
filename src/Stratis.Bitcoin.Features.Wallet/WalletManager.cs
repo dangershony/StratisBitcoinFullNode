6bee8439cdf03d7afc5a6f98dfc97215196e5dfa
Kees de Wit <keesdewit@keesdewit.nl>
2019-10-17 12:02:17 +0200

    Use GetUnusedAddresses to generate a new address

--- src/Stratis.Bitcoin.Features.Wallet/WalletManager.cs
+++ src/Stratis.Bitcoin.Features.Wallet/WalletManager.cs
@@ -595,7 +595,7 @@ namespace Stratis.Bitcoin.Features.Wallet
         }
 
         /// <inheritdoc />
-        public IEnumerable<HdAddress> GetUnusedAddresses(WalletAccountReference accountReference, int count, bool isChange = false)
+        public IEnumerable<HdAddress> GetUnusedAddresses(WalletAccountReference accountReference, int count, bool isChange = false, bool alwaysnew = false)
         {
             Guard.NotNull(accountReference, nameof(accountReference));
             Guard.Assert(count > 0);
@@ -604,6 +604,8 @@ namespace Stratis.Bitcoin.Features.Wallet
 
             bool generated = false;
             IEnumerable<HdAddress> addresses;
+            
+            var newAddresses = new List<HdAddress>();
 
             lock (this.lockObject)
             {
@@ -616,8 +618,8 @@ namespace Stratis.Bitcoin.Features.Wallet
                     account.InternalAddresses.Where(acc => !acc.Transactions.Any()).ToList() :
                     account.ExternalAddresses.Where(acc => !acc.Transactions.Any()).ToList();
 
-                int diff = unusedAddresses.Count - count;
-                var newAddresses = new List<HdAddress>();
+                int diff = alwaysnew ? -1 : unusedAddresses.Count - count;
+                
                 if (diff < 0)
                 {
                     newAddresses = account.CreateAddresses(this.network, Math.Abs(diff), isChange: isChange).ToList();
@@ -632,6 +634,8 @@ namespace Stratis.Bitcoin.Features.Wallet
             {
                 // Save the changes to the file.
                 this.SaveWallet(wallet);
+
+                return newAddresses;
             }
 
             return addresses;
